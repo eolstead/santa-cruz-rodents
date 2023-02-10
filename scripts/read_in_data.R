74d1f3865a770125eb25e5d051dc7898d17c16e2
Ellen Bledsoe <ellen.bledsoe@weecology.org>
2023-02-10 09:47:21 -0700

    add structure to repo and save raw data files

--- scripts/read_in_data.R
+++ scripts/read_in_data.R
@@ -4,12 +4,14 @@
 
 # PACKAGES ####
 
-install.packages("googlesheets4") # run this line of code only once
+#install.packages("googlesheets4") # run this line of code only once
 library(googlesheets4) # need to run this line every time you run this script
+library(tidyverse)
 
 # DATA ####
 
 microsite <- read_sheet("https://docs.google.com/spreadsheets/d/11OT2-G6UCpaIVKsEvykI1HtpqDd9xdFLZJoLI3cf86g/edit#gid=0")
 capture <- read_sheet("https://docs.google.com/spreadsheets/d/11OT2-G6UCpaIVKsEvykI1HtpqDd9xdFLZJoLI3cf86g/edit#gid=0", sheet = 2)
 
-
+write_csv(microsite, "data/microsite_raw.csv")
+write_csv(capture, "data/capture_raw.csv")
